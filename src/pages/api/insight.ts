b1a3cf535f6dfcfcc4e3ce153d84d5eeb1989053
k-logic24 <49090551+k-logic24@users.noreply.github.com>
2022-05-15 19:26:49 +0900

    リファラー制限

--- src/pages/api/insight.ts
+++ src/pages/api/insight.ts
@@ -11,7 +11,11 @@ type Props = types.api.Response | types.api.ErrorResponse
 const getData = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const data = await http.get<Props>(
-      `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${req.query.url}&strategy=${req.query.strategy}&key=${key}`
+      `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${req.query.url}&strategy=${req.query.strategy}&key=${key}`, {
+        headers: {
+          'Referer': `${req.headers.referer}`
+        }
+      }
     )
     return res.status(200).json(data.data)
   } catch (e) {
