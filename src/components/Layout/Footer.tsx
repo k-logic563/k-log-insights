884cab77a1f2f408159de2eba6a39d392db44516
k-logic24 <k.log.ic563@gmail.com>
2022-07-03 16:42:02 +0900

    copyrightを変更

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <div className="py-3 bg-white text-center">
-      <small>&copy; {new Date().getFullYear()} KLOG Insights</small>
+      <small>&copy; {new Date().getFullYear()} klog-insights</small>
     </div>
   )
 }
